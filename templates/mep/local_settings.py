4d2c4c7302917860c3c1a46eadfa39d55d4612ae
Benjamin Hicks <benjamin.w.hicks@gmail.com>
2018-09-14 13:50:33 -0400

    Add complete mep playbooks and vars

--- templates/mep/local_settings.py
+++ templates/mep/local_settings.py
@@ -3,4 +3,24 @@
 {% block extra_config %}
 {# Add static root as needed now because missing from settings.py as of 0.7 #}
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# Separate Geonames for usage reasons
+GEONAMES_USERNAME = '{{ geonames_username }}'
+# Mapbox token shared with Winthrop
+MAPBOX_ACCESS_TOKEN = '{{ mapbox_token }}'
+{% endblock %}
+
+{% block logging %}
+# Solution following https://stackoverflow.com/a/9541647
+# Sends a logging email even when DEBUG is on
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': True,
+        },
+    },
+}
 {% endblock %}
