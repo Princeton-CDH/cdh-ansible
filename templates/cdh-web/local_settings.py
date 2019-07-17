b0ced9b16c7574d3a5d327d6462f378199134602
Benjamin Hicks <benjamin.w.hicks@gmail.com>
2019-07-17 14:42:04 -0400

    Update logging settings for cdhweb to standard

--- templates/cdh-web/local_settings.py
+++ templates/cdh-web/local_settings.py
@@ -9,13 +9,39 @@ NEVERCACHE_KEY = "{{ nevercache_key }}"
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'basic': {
+            'format': '[%(asctime)s] %(levelname)s:%(name)s::%(message)s',
+            'datefmt': '%d/%b/%Y %H:%M:%S',
+        },
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
             'class': 'django.utils.log.AdminEmailHandler',
-            'include_html': True,
+            'include_html': True
         },
+        {# This configuration lets logrotate and its proper permissions #}
+        {# handle this problem #}
+        'debug_log': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.WatchedFileHandler',
+            'filename': '{{ logging_path }}',
+            'formatter': 'basic',
+        }
     },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins', 'debug_log'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'cdhweb': {
+            'handlers': ['debug_log'],
+            'level': 'WARN',
+            'propagate': True,
+        },
+    }
 }
 {% endblock %}
 
