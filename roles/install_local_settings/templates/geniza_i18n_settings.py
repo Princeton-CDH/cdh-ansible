177eb353b0ae5b509f24c60d3dde968a96f42d4f
Nick Budak <nbudak@princeton.edu>
2021-02-01 17:16:37 -0500

    Add pucas config for i18n qa

--- roles/install_local_settings/templates/geniza_i18n_settings.py
+++ roles/install_local_settings/templates/geniza_i18n_settings.py
@@ -1,9 +1,29 @@
-{% extends 'settings.py' %}
+{% extends "settings.py" %}
 """
 Django settings for geniza i18n prototype.
 """
 
 {% block cas_config %}
+CAS_SERVER_URL = "https://fed.princeton.edu/cas/"
+
+CAS_VERSION = "CAS_2_SAML_1_0"
+
+PUCAS_LDAP = {
+    "ATTRIBUTES": ["givenName", "sn", "mail"],
+    "ATTRIBUTE_MAP": {
+        "first_name": "givenName",
+        "last_name": "sn",
+        "email": "mail",
+    },
+    "SERVERS": [
+        "ldap2.princeton.edu",
+        "ldap3.princeton.edu",
+        "ldap4.princeton.edu",
+        "ldap5.princeton.edu"
+    ],
+    "SEARCH_BASE": "o=Princeton University,c=US",
+    "SEARCH_FILTER": "(uid=%(user)s)",
+}
 {% endblock %}
 
 {% block extra_config %}
