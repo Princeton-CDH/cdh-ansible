8abfc915cea6198e295a3d320b63074ccf3d15a0
Nick Budak <nbudak@princeton.edu>
2021-03-15 14:44:43 -0400

    Make geniza localsettings use default CAS auth block

--- roles/install_local_settings/templates/geniza_settings.py
+++ roles/install_local_settings/templates/geniza_settings.py
@@ -3,29 +3,6 @@
 Django settings for geniza
 """
 
-{% block cas_config %}
-CAS_SERVER_URL = "https://fed.princeton.edu/cas/"
-
-CAS_VERSION = "CAS_2_SAML_1_0"
-
-PUCAS_LDAP = {
-    "ATTRIBUTES": ["givenName", "sn", "mail"],
-    "ATTRIBUTE_MAP": {
-        "first_name": "givenName",
-        "last_name": "sn",
-        "email": "mail",
-    },
-    "SERVERS": [
-        "ldap2.princeton.edu",
-        "ldap3.princeton.edu",
-        "ldap4.princeton.edu",
-        "ldap5.princeton.edu"
-    ],
-    "SEARCH_BASE": "o=Princeton University,c=US",
-    "SEARCH_FILTER": "(uid=%(user)s)",
-}
-{% endblock %}
-
 {% block extra_config %}
 # Use x-forwarded-proto header to tell if request from nginx was https or not
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
