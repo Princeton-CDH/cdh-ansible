3573edc9d2056b05193a4666105c3c8906fc4fd5
Kevin McElwee <kevinrmcelwee@gmail.com>
2021-10-28 17:12:15 -0400

    Add custom driver

--- roles/browsertrix/files/custom-driver.js
+++ roles/browsertrix/files/custom-driver.js
@@ -1,3 +1,9 @@
 module.exports = async ({ data, page, crawler }) => {
     await crawler.loadPage(page, data);
+
+    const markers = await page.$$('.visualization-chapter-marker')
+    for (let count = 0; count < markers.length; count++) {
+        await markers[count].hover();
+        await crawler.sleep(1000);
+    }
 };
