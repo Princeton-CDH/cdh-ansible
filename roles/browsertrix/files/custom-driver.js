fd4888d5ebea1289c2012ab4806d3143f71e5b94
Kevin McElwee <kevinrmcelwee@gmail.com>
2021-10-29 10:06:21 -0400

    Get deep zoom image

--- roles/browsertrix/files/custom-driver.js
+++ roles/browsertrix/files/custom-driver.js
@@ -1,9 +1,17 @@
 module.exports = async ({ data, page, crawler }) => {
-    await crawler.loadPage(page, data);
+  await crawler.loadPage(page, data);
 
-    const markers = await page.$$('.visualization-chapter-marker')
-    for (let count = 0; count < markers.length; count++) {
-        await markers[count].hover();
-        await crawler.sleep(1000);
-    }
+  const markers = await page.$$('.visualization-chapter-marker')
+  for (let count = 0; count < markers.length; count++) {
+    await markers[count].hover();
+    await crawler.sleep(1000);
+  }
+
+  // get deep zoom info.json
+  const zoom_button = await page.$$('[href="#zoom"]');
+  if (zoom_button.length > 0) {
+    await crawler.sleep(1000);
+    await zoom_button[0].click()
+    await crawler.sleep(1000);
+  }
 };
