ee7b47fc26fb453ef422e749ab433fef490cdb8c
rlskoeser <rebecca.s.koeser@princeton.edu>
2021-11-04 12:37:50 -0400

    Add more comments to derrida browsertrix driver

--- roles/browsertrix/files/derrida-driver.js
+++ roles/browsertrix/files/derrida-driver.js
@@ -1,13 +1,17 @@
+// custom driver for archiving Derrida's Margins
 module.exports = async ({ data, page, crawler }) => {
   await crawler.loadPage(page, data);
 
+  // for the interactive histogram viz, hover over each marker
+  // to load & archive the ajax reference card
   const markers = await page.$$('.visualization-chapter-marker')
   for (let count = 0; count < markers.length; count++) {
     await markers[count].hover();
     await crawler.sleep(1000);
   }
 
-  // get deep zoom info.json
+  // for image display, toggle to deep zoom mode to
+  // load & archive local IIIF info.json
   const zoom_button = await page.$$('[href="#zoom"]');
   if (zoom_button.length > 0) {
     await crawler.sleep(1000);
